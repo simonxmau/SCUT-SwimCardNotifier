9a47004a4bf467dd23c12445f6cd01c0b9c794dd
Simon Mau <git@simonmau.me>
2024-09-09 10:05:57 +0800

    perf: 保持会话时间延长

--- app/config/celery_beat_schedule.py
+++ app/config/celery_beat_schedule.py
@@ -3,7 +3,7 @@ from celery.schedules import crontab
 BEAT_SCHEDULE = {
     'add-every-1-minute': {
         'task': 'tasks.keep_session_to_live',
-        'schedule': crontab(minute='*/5'),
+        'schedule': crontab(minute='*/15'),
     },
     'add-every-30-seconds': {
         'task': 'tasks.cron_check',
