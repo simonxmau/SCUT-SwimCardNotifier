362842d0ca742b95ce6368f3dad7db8579c6caae
Simon Mau <git@simonmau.me>
2024-09-14 13:07:30 +0800

    chore: remove the work area files

--- synccode.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-scp -r app dev1:/data/docker/SCUT-SwimCardNotifier
-scp -r docker dev1:/data/docker/SCUT-SwimCardNotifier
-scp -r docker-compose.yml dev1:/data/docker/SCUT-SwimCardNotifier
-scp -r Dockerfile dev1:/data/docker/SCUT-SwimCardNotifier
-scp -r main.py dev1:/data/docker/SCUT-SwimCardNotifier
