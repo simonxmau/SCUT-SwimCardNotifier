4da4f8e210c8112569a3eb9563a8f72784e40f53
Simon Mau <git@simonmau.me>
2024-09-20 23:51:32 +0800

    feat: 订单号打码

--- app/lib/bot.py
+++ app/lib/bot.py
@@ -12,11 +12,11 @@ logger.info(f'读取配置: webhook={webhook}, secret={secret}')
 
 
 def send(order_no, remain, time, at_id='', at_all=False):
-    title = f'{order_no} 剩余 {remain} 次'
+    title = f'{mask_order_number(order_no)} 剩余 {remain} 次'
     text = f'''
 ### **订单次数变动提醒**
 
-**订单号码:** {order_no}
+**订单号码:** {mask_order_number(order_no)}
 
 **剩余次数:** {remain}
 
@@ -39,7 +39,26 @@ def send_error(message, order_no, time):
     res = bot.send_markdown(
         title=f'查询异常',
         text=f'### **{message}**\n'
-             f'**订单号码:**  {order_no}\n\n'
+             f'**订单号码:**  {mask_order_number(order_no)}\n\n'
              f'**通知时间:**  {time}\n\n',
         is_at_all=True)
     logger.info(f'推送结果: message={message}, order_no={order_no}, time={time}, result={res}')
+
+
+def mask_order_number(order_number, mask_char='*', visible_digits=4):
+    """
+    对订单号码进行打码处理，保留头4位和尾4位数字以便辨识。
+
+    :param order_number: 订单号码（字符串形式）
+    :param mask_char: 用于打码的字符，默认为 '*'
+    :param visible_digits: 保留的可见数字位数，默认为 4
+    :return: 打码后的订单号码
+    """
+    if not isinstance(order_number, str):
+        order_number = str(order_number)
+
+    head_visible_part = order_number[:visible_digits]
+    tail_visible_part = order_number[-visible_digits:]
+    masked_part = mask_char * (len(order_number) - 2 * visible_digits)
+
+    return head_visible_part + masked_part + tail_visible_part
